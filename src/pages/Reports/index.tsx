c08e464f3d53d1fd56336b6eb1afb9737ee909d8
prabodha <prabolenora@gmail.com>
2021-10-21 12:18:50 +0530

    report

--- src/pages/Reports/index.tsx
+++ src/pages/Reports/index.tsx
@@ -1,11 +1,90 @@
 import React from 'react'
-
+import './Report.scss'
 type Props = {}
+
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const data = [
+    {
+      name: 'Jan',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Feb',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'March',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'April',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'May',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'June',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'July',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+    {
+      name: 'Aug',
+      uv: 3590,
+      pv: 4600,
+      amt: 2100,
+    },
+    {
+      name: 'Sep',
+      uv: 1490,
+      pv: 1300,
+      amt: 2100,
+    },
+  ];
 
 export default function Reports(props: Props): JSX.Element {
     return (
-        <div>
-            <h2>This is report page</h2>
-        </div>
+        <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        </LineChart>
+      </ResponsiveContainer>
+
+      
     )
 }
