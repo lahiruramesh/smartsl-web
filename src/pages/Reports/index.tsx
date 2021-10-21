9025a1aeaa2e4cca712a5f1e27b404e58f871b21
prabodha <prabolenora@gmail.com>
2021-10-22 01:59:01 +0530

    report

--- src/pages/Reports/index.tsx
+++ src/pages/Reports/index.tsx
@@ -1,71 +1,154 @@
-import React from 'react'
-import './Report.scss'
-type Props = {}
-
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
+import React from "react";
+import "./Report.scss";
+import {LineChart,  Line,  XAxis,  YAxis,  CartesianGrid,  Tooltip,  Legend,  ResponsiveContainer,} from "recharts";
+import { Button,  Card,  Checkbox,  Form,  Input,  Layout,  Row,  Col,  Table,} from "antd";
+type Props = {};
 const data = [
-    {
-      name: 'Jan',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Feb',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'March',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'April',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'May',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'June',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'July',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-    {
-      name: 'Aug',
-      uv: 3590,
-      pv: 4600,
-      amt: 2100,
-    },
-    {
-      name: 'Sep',
-      uv: 1490,
-      pv: 1300,
-      amt: 2100,
-    },
-  ];
+  {
+    name: "Jan",
+    income: 4000,
+    expense: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Feb",
+    income: 3000,
+    expense: 1398,
+    amt: 2210,
+  },
+  {
+    name: "March",
+    income: 2000,
+    expense: 9800,
+    amt: 2290,
+  },
+  {
+    name: "April",
+    income: 2780,
+    expense: 3908,
+    amt: 2000,
+  },
+  {
+    name: "May",
+    income: 1890,
+    expense: 4800,
+    amt: 2181,
+  },
+  {
+    name: "June",
+    income: 2390,
+    expense: 3800,
+    amt: 2500,
+  },
+  {
+    name: "July",
+    income: 3490,
+    expense: 4300,
+    amt: 2100,
+  },
+  {
+    name: "Aug",
+    income: 3590,
+    expense: 4600,
+    amt: 2100,
+  },
+  {
+    name: "Sep",
+    income: 1490,
+    expense: 1300,
+    amt: 2100,
+  },
+];
+
+const dataSource = [
+  {
+    key: 'month',
+    month: 'Expected income %',
+    january: 32,
+    february: 54,
+    march:54,
+    april: 32,
+    may: 54,
+    june:54,
+    july: 38,
+    august: 64,
+    september:95,
+
+  },
+  {
+    key: '2',
+    month: 'Expected expense %',
+    january: 32,
+    february: 54,
+    march:54,
+    april: 32,
+    may: 54,
+    june:54,
+    july: 38,
+    august: 64,
+    september:95,
+  },
+];
+
+const columns = [
+  {
+    title: 'Month',
+    dataIndex: 'month',
+    key: 'month',
+  },
+  {
+    title: 'January',
+    dataIndex: 'january',
+    key: 'january',
+  },
+  {
+    title: 'February',
+    dataIndex: 'february',
+    key: 'february',
+  },
+  {
+    title: 'March',
+    dataIndex: 'march',
+    key: 'march',
+  },
+  {
+    title: 'April',
+    dataIndex: 'april',
+    key: 'april',
+  },
+  {
+    title: 'May',
+    dataIndex: 'may',
+    key: 'may',
+  },
+  {
+    title: 'June',
+    dataIndex: 'june',
+    key: 'june',
+  },
+  {
+    title: 'July',
+    dataIndex: 'july',
+    key: 'july',
+  },
+  {
+    title: 'August',
+    dataIndex: 'august',
+    key: 'august',
+  },
+  {
+    title: 'September',
+    dataIndex: 'september',
+    key: 'september',
+  },
+];
 
 export default function Reports(props: Props): JSX.Element {
-    return (
-        <ResponsiveContainer width="100%" height="100%">
+  return (
+    <><div>
+      <Card style={{ width: '100%', height: "60%" }}>
         <LineChart
-          width={500}
+          width={1000}
           height={300}
           data={data}
           margin={{
@@ -80,11 +163,20 @@ export default function Reports(props: Props): JSX.Element {
           <YAxis />
           <Tooltip />
           <Legend />
-          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+          <Line
+            type="monotone"
+            dataKey="expense"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="income" stroke="#82ca9d" />
         </LineChart>
-      </ResponsiveContainer>
+      </Card>
+    </div><div>
+        <Card style={{ width: '100%', height: "60%" }}>
+        <Table dataSource={dataSource} columns={columns} />;
+        </Card>
+      </div></>
+  
 
-      
-    )
+  );
 }
